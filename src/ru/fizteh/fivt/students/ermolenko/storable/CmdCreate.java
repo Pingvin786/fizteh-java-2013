f28984999c5d22f9b0474cc1f7cda16f9e2b9e56
Pingvin786 <pingvin786@mail.ru>
2013-11-15 03:04:59 +0400

    fixed tests

--- src/ru/fizteh/fivt/students/ermolenko/storable/CmdCreate.java
+++ src/ru/fizteh/fivt/students/ermolenko/storable/CmdCreate.java
@@ -20,7 +20,9 @@ public class CmdCreate implements Command<StoreableState> {
         if (args.length != 2) {
             throw new IOException("incorrect number of arguments");
         }
-
+        if (args[1].trim() == "") {
+            throw new IOException("wrong type");
+        }
         List<Class<?>> columnTypes = new ArrayList<Class<?>>();
 
         String[] types = args[1].trim().split("\\s+");
